3bbb803aa4c8fdf8ecb18b3ef5d9e55bf7a0d1e0
Teimuraz Shengelia <teimurazshengelia@credo.ge>
2024-10-29 18:05:16 +0400

    Add project files.

---
+++ davaleba4/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] students = new string[3]; 
+for (int i = 0; i < students.Length; i++) 
+{ string name = Console.ReadLine(); 
+  students[i] = name; 
+}
+for (int i = 0; i < students.Length; i++) 
+{
+    Console.WriteLine("sudents");
+    Console.WriteLine(students[i]); 
+}
+
+
